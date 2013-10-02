123187ff7f590ef2ff10bcb789aeab9ea1a96bf0
NicholasBallard <nicholasericballard@gmail.com>
2013-10-02 22:01:32 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Practu::Application.config.secret_key_base = '34319af845c6f33c440e17b90671278dc889bba3f78eab58486e5930a9ab08b883dafeca965ef6a49c0f5b5e317a2b4171800cbd9fcba2a380dd7fd89de44974'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Practu::Application.config.session_store :cookie_store, key: '_practu_session'
